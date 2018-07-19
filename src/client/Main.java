d9ca0ac944c58bd6af8b6a982b30f58cb56375e6
siriusonestar <siriusonestar@yandex.ru>
2018-07-20 01:21:21 +0300

    first commit

---
+++ src/client/Main.java
@@ -0,0 +1,10 @@
+package client;
+
+import client.model.ClientWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        ClientWindow clientWindow = new ClientWindow();
+    }
+
+}
---
+++ src/server/ClientHandler.java
@@ -0,0 +1,67 @@
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientHandler implements Runnable{
+    private Server server;
+    private PrintWriter outMessage;
+    private Scanner inMessage;
+    private static final String HOST = "Localhost";
+    private static final int PORT = 8080;
+
+    private Socket clientSocket = null;
+    private static int clients_count = 0;
+
+    public ClientHandler(Socket socket, Server server){
+        try {
+            clients_count++;
+            this.server = server;
+            this.clientSocket = socket;
+            this.outMessage = new PrintWriter(socket.getOutputStream());
+            this.inMessage = new Scanner(socket.getInputStream());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void run(){
+        try {
+            while (true){
+                server.sendMessageToAllClients("Новый участник в чате");
+                server.sendMessageToAllClients("Клиентов в чате = " + clients_count);
+                break;
+            }
+            while (true){
+                if (inMessage.hasNext()){
+                    String clientMessage = inMessage.nextLine();
+                if (clientMessage.equalsIgnoreCase("##session##end##")){
+                        break;
+                    }
+                    System.out.println(clientMessage);
+                server.sendMessageToAllClients(clientMessage);
+                }
+                Thread.sleep(100);
+            }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            this.close();
+        }
+    }
+    public void sendMsg(String msg){
+        try{
+            outMessage.println(msg);
+            outMessage.flush();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void close(){
+        server.removeClient(this);
+        clients_count--;
+        server.sendMessageToAllClients("Клиентов в чате = " + clients_count);
+    }
+}
